221b1115f305a965a1c027c233221c1bce50f8ee
Luis Gabriel Sanchez <gabykosten@gmail.com>
2023-06-26 19:21:40 -0300

    Segundo Inicio

--- app.js
+++ app.js
@@ -1,3 +1,143 @@
+function ingreseDatos() {
+  //Función general.
+  function nombre() {
+    //Función particular.
+    let nombreUsuario = prompt("Señor usuario, ingrese Luis.");
+    if (nombreUsuario == "Luis" || nombreUsuario == "luis") {
+      alert("El nombre ingresado es correcto.");
+    } else {
+      while (nombreUsuario != "Luis" || nombreUsuario != "luis") {
+        alert("El dato ingresado no es correcto");
+        nombreUsuario = prompt("Señor usuario, ingrese Luis o luis.");
+      }
+    }
+  }
+  nombre();
+  function apellido() {
+    let apellidoUsuario = prompt("Señor usuario, ingrese su Kosten.");
+    if (apellidoUsuario == "Kosten" || apellidoUsuario == "kosten") {
+      alert("El apellido ingresado es correcto.");
+    } else {
+      while (apellidoUsuario != "Kosten" || apellidoUsuario != "kosten") {
+        alert("El dato ingresado no es correcto");
+        apellidoUsuario = prompt("Señor usuario, ingrese Kosten o kosten.");
+      }
+    }
+  }
+  apellido();
+  function email() {
+    let emailUsuario = prompt(
+      "Señor usuario, ingrese su luis.kosten@gmail.com"
+    );
+    if (emailUsuario == "luis.kosten@gmail.com") {
+      alert("El E-mail ingresado es correcto.");
+    }
+    while (emailUsuario != "luis.kosten@gmail.com") {
+      alert("El dato ingresado no es correcto");
+      emailUsuario = prompt("Señor usuario, ingrese luis.kosten@gmail.com");
+    }
+  }
+  email();
+  function contraseña() {
+    let contraseñaUsuario = prompt("Señor usuario, ingrese la cotraseña: 1234.1234");
+    if (contraseñaUsuario == "1234.1234") {
+      alert("La contraseña ingresada es correcta.");
+      alert("Binenvenido.");
+    } else {
+      while (contraseñaUsuario != "1234.1234") {
+        alert("El dato ingresado no es correcto");
+        contraseñaUsuario = prompt("Señor usuario, ingrese la cotraseña: 1234.1234");
+      }
+    }
+  }
+  contraseña();
+}
+
+ingreseDatos();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*function ingreseDatos() {
   let nombreUsuario = prompt("Señor usuario, ingrese Luis.");
   if (nombreUsuario == "Luis" || nombreUsuario == "luis") {
@@ -78,60 +218,3 @@
 // }
 
 // ingreseDatos();
-
-function ingreseDatos() {
-  //Función general.
-  function nombre() {
-    //Función particular.
-    let nombreUsuario = prompt("Señor usuario, ingrese Luis.");
-    if (nombreUsuario == "Luis" || nombreUsuario == "luis") {
-      alert("El nombre ingresado es correcto.");
-    } else {
-      while (nombreUsuario != "Luis" || nombreUsuario != "luis") {
-        alert("El dato ingresado no es correcto");
-        nombreUsuario = prompt("Señor usuario, ingrese Luis o luis.");
-      }
-    }
-  }
-  nombre();
-  function apellido() {
-    let apellidoUsuario = prompt("Señor usuario, ingrese su Kosten.");
-    if (apellidoUsuario == "Kosten" || apellidoUsuario == "kosten") {
-      alert("El apellido ingresado es correcto.");
-    } else {
-      while (apellidoUsuario != "Kosten" || apellidoUsuario != "kosten") {
-        alert("El dato ingresado no es correcto");
-        apellidoUsuario = prompt("Señor usuario, ingrese Kosten o kosten.");
-      }
-    }
-  }
-  apellido();
-  function email() {
-    let emailUsuario = prompt(
-      "Señor usuario, ingrese su luis.kosten@gmail.com"
-    );
-    if (emailUsuario == "luis.kosten@gmail.com") {
-      alert("El E-mail ingresado es correcto.");
-    }
-    while (emailUsuario != "luis.kosten@gmail.com") {
-      alert("El dato ingresado no es correcto");
-      emailUsuario = prompt("Señor usuario, ingrese luis.kosten@gmail.com");
-    }
-  }
-  email();
-  function contraseña() {
-    let contraseñaUsuario = prompt("Señor usuario, ingrese la cotraseña: 1234.1234");
-    if (contraseñaUsuario == "1234.1234") {
-      alert("La contraseña ingresada es correcta.");
-      alert("Binenvenido.");
-    } else {
-      while (contraseñaUsuario != "1234.1234") {
-        alert("El dato ingresado no es correcto");
-        contraseñaUsuario = prompt("Señor usuario, ingrese la cotraseña: 1234.1234");
-      }
-    }
-  }
-  contraseña();
-}
-
-ingreseDatos();
